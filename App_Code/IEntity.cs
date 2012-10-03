efd8d3eebcde39287aa8dbff4f9f33a7494303f6
Rob Attfield <attfire1@student.op.ac.nz>
2012-10-04 00:13:43 +1300

    test commit

---
+++ App_Code/IEntity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Interface holding common methods inside the 
+/// classes that implement it
+/// </summary>
+public interface IEntity<T>
+{
+    public void Add(T item);
+    public void Get(ref List<T> item);
+    public void Get(ref T item, int id);
+    public void Update(T item, int id);
+    public void Delete(int id);
+}
